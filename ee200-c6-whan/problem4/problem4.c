d04028310d682bd4e7d7963e69d6bc2e991cce79
wjhan1993 <43017036+wjhan1993@users.noreply.github.com>
2018-10-19 12:39:49 -0400

    Update problem4.c

--- ee200-c6-whan/problem4/problem4.c
+++ ee200-c6-whan/problem4/problem4.c
@@ -5,35 +5,37 @@
 
 const char* find_shortest(const char * const * strings, int len)
 {
-  // if strings is NULL, exit without crash
+  // if strings is NULL, return NULL
   if (strings == NULL)
     return(NULL);
   
-  int i;
+  int row;
   int length = 0;
   const char *shortStr = NULL;
   int shortest = -1;
   
-  for(i = 0;i < len;i++){
+  for(row = 0;row < len;row++){
+    // length is the current string length
     length = 0;
     
-    if (strings[i] != NULL)
-      {
-        // calculate the length of string[i]
+    // If the current string is not Null, calculate the length of string[i]
+    if (strings[row] != NULL)
+    {
         while(strings[i][length] != '\0')
           length++;
-      }
+    }
     else
       continue;
     
-    // check whether string[i] is shorter than the current shortest string
-    // if yes, update shortest and the pointer shortStr
+    // initialize shortest as the first non-null string's length
     if (shortest == -1)
     {
       shortest = length;
       shortStr = strings[i];
     }
    
+    // check whether string[i] is shorter than the current shortest string
+    // if yes, update shortest and the pointer shortStr
     if(length < shortest){
       shortest = length;
       shortStr = strings[i];
